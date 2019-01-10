44c1bcdf3215da0fda103368e5008e4661351f81
jared <jaredpattison@users.noreply.github.com>
2019-01-10 13:25:50 -0800

    daily

--- index.js
+++ index.js
@@ -2,40 +2,38 @@
 
 let arr = [];
 
-for(let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   arr.push(i);
 }
 
 function forLoop(arr) {
-  
-  for(let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
   }
 }
 
 function whileLoop(arr) {
   let i = 0;
-  
   while (i < arr.length) {
     console.log(arr[i]);
-    i++;
+    i++; 
   }
 }
 
-function map (arr, cb) {
+function map(arr, cb) {
   let newArr = [];
-  
-  for(let i = 0; i < arr.length; i++) {
-    newArr.push( cb(arr[i], i) );
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i], i)) {
+      newArr.push(arr[i]);
+    }
   }
   return newArr;
 }
 
 function filter(arr, cb) {
   let newArr = [];
-  
   for (let i = 0; i < arr.length; i++) {
-    if( cb(arr[i], i)) {
+    if( cb(arr[i], i))  {
       newArr.push(arr[i]);
     }
   }
@@ -43,13 +41,14 @@ function filter(arr, cb) {
 }
 
 function reduce(start, arr, cb) {
-  let accumulator = start;
- 
-  for(let i = 0; i < arr.length; i++) {
-    accumulator += cb(start, arr[i], i, arr);
+  let acc = start;
+  for (let i= 0; i < arr.length; i++) {
+    acc += cb(start, arr[i], i, arr);
   }
-  return accumulator;
-}const people = ['Kookla','Fran','Ollie'];
+  return acc;
+}
+
+const people = ['Kookla','Fran','Ollie'];
 
 const stuff = {
   tv: 'huge',
@@ -61,12 +60,5 @@ const stuff = {
 let state = { people: [...people], ...stuff};
 
 let newPeople = [ 'Odie', ...people, 'Garfield'];
-const newStuff = { ...stuff, cars: [ ...stuff.cars, 'Porche']};
-let newState = { ...state, people: ['Odie', ...state.people, 'Garfield'], stuff: [ ...stuff.cars, 'Porche']};
-
-console.log('people', people);
-console.log('stuff', stuff);
-console.log('state', state);
-console.log('newPeople', newPeople);
-console.log('newStuff', newStuff);
-console.log('newState', newState);
+const newStuff = { ...stuff, cars: [ ...stuff.cars, 'Mazdarati']};
+let newState = { ...state, people: ['Odie', ...state.people, 'Garfield'], stuff: [...stuff.cars, 'Masdarati']};

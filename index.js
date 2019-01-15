9823d4eb71ceb1be5e6682b481d3f57de76e7680
jared <jaredpattison@users.noreply.github.com>
2019-01-15 12:45:19 -0800

    daily

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let numbers = [1,2,3,4,5,6,7,8,9,10];
 const people = ['Kookla','Fran','Ollie'];
 const stuff = {
   tv: 'huge',
@@ -8,64 +9,90 @@ const stuff = {
   cars: ['Toyota','Mazda'],
 };
 
-let arr = [];
-
-for (let i = 0; i < 10; i++) {
-  arr.push(i);
-}
-
 function forLoop(arr) {
-  for (let i = 0; i < arr.length; i++) {
+  for(let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
   }
 }
 
 function whileLoop(arr) {
-  let i = 0;
-  while (i < arr.length) {
-    console.log(arr[i]);
-    i++; 
+  let array = [...arr];
+  while(array.length) {
+    console.log(array.shift())
   }
 }
 
 function map(arr, cb) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (cb(arr[i], i)) {
-      newArr.push(arr[i]);
-    }
+  let newArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    newArray.push(cb(arr[i]));
   }
-  return newArr;
+  return newArray;
 }
 
 function filter(arr, cb) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if( cb(arr[i], i))  {
-      newArr.push(arr[i]);
+  let newArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(cb(arr[i])) {
+      newArray.push(arr[i]);
     }
   }
-  return newArr;
+  return newArray;
 }
 
 function reduce(arr, cb, newThing) {
-  for (let i= 0; i < arr.length; i++) {
-    newThing = cb(newThing, arr[i], i, arr);
+  for(let i = 0; i < arr.length; i++) {
+    newThing = cb(newThing, arr[i], i);
   }
   return newThing;
 }
 
-const people = ['Kookla','Fran','Ollie'];
+console.log('forLoop()');
+forLoop(numbers);
+console.log('------------------\n\n');
 
-const stuff = {
-  tv: 'huge',
-  radio: 'old',
-  toothbrush: 'frayed',
-  cars: ['Toyota','Mazda']
-}
+console.log('whileLoop()');
+whileLoop(numbers);
+console.log('------------------\n\n');
 
-let state = { people: [...people], ...stuff};
+console.log('map()');
+let squares = map(numbers, (val=>val * val));
+console.log({squares});
+console.log('------------------\n\n');
 
+console.log('filter()');
+let odds = filter(numbers, (val => !!(val % 2)));
+console.log({odds});
+console.log('------------------\n\n');
+
+console.log('reduce()');
+let sum = reduce(numbers, (acc,num) => {
+  acc += num;
+  return acc;
+},0);
+
+console.log({sum});
+console.log('------------------\n\n');
+
+const state = {people, stuff};
 let newPeople = [ 'Odie', ...people, 'Garfield'];
-const newStuff = { ...stuff, cars: [ ...stuff.cars, 'BMW']};
-let newState = { ...state, people: ['Odie', ...state.people, 'Garfield'], stuff: [...stuff.cars, 'BMW']};
+const newStuff = {...stuff, cars:[...stuff.cars, 'BMW']};
+let newState = {...state, people: ['Odie', ...people, 'Garfield'], stuff:{...stuff, cars:[...stuff.cars, 'BMW']}};
+
+console.log({people});
+console.log('------------------\n\n');
+
+console.log({newPeople});
+console.log('------------------\n\n');
+
+console.log({stuff});
+console.log('------------------\n\n');
+
+console.log({newStuff});
+console.log('------------------\n\n');
+
+console.log({state});
+console.log('------------------\n\n');
+
+console.log({newState});
+console.log('------------------\n\n');
